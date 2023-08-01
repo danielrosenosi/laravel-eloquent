fbc28f55426dcb621340d898fdaf57135538170f
danielrosenosi <devroseno@gmail.com>
2023-08-01 16:37:10 -0300

    feat: exploring model properties

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // protected $table = 'nome_da_tabela'; define o nome da tabela, se não for o nome da model no plural
+    // protected $timestamps = false; desabilita as colunas timestamps
+    // protected $connection = 'pgsql';  altera a conexão à nível de model
+    // protected $primaryKey = 'nome_da_chave_primaria'; por default o laravel busca por id
+    // protected $keyType = 'string'; altera o tipo da chave primária, se caso não for integer
+    // protected $incrementing = false; desabilita incrementação no id
+    // const CREATED_AT = 'data_criacao';
+    // const UPDATED_AT = 'data_atualizacao';
+    // protected $dateFormat = 'd/m/y';
+    // protected $attributes = ['coluna' => 'valor'];
 }
